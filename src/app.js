cceba83403bfc0d97055e13260bd5e860e2093b6
Patrick Nandom <patricknandom82@gmail.com>
2024-10-20 14:35:43 +0100

    made correction on the models

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const router = require("./routes/index");
+const cors = require("cors");
 
 // LOAD ENVIRONMENT VARIABLES
 dotenv.config();
 
 // INITIALIZE EXPRESS
 const app = express();
+app.use(cors());
 
 // MIDDLEWARE
 app.use(express.json());
